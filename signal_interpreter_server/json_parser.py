b99f58f5d2c3ab1f65fb5fed45978616feceef37
Dykin <VDYKIN@volvocars.com>
2021-02-12 07:54:49 +0100

    21 commit

--- signal_interpreter_server/json_parser.py
+++ signal_interpreter_server/json_parser.py
@@ -1,9 +1,12 @@
 # json_parser.py
 # pylint: disable=missing-class-docstring
 
+import logging
 import json
 from flask import abort
 from signal_interpreter_server.exceptions import JsonParserError
+
+logger = logging.getLogger(__name__)
 
 
 class JsonParser:
@@ -12,12 +15,17 @@ class JsonParser:
 
     def load_file(self, file_path):
         # open the json file
+        logger.info("Opening json database file: %s", file_path)
         try:
             with open(file_path, "r") as json_file:
                 # load the json file and save it to self.data
+                logger.info("Loading json database file: %s", file_path)
                 self.data = json.load(json_file)
                 return self.data
         except FileNotFoundError as err:
+            # logger.exception("Exception occurred %s ", err)
+            logger.warning("Exception occurred %s ", err)
+            logger.info("Raising exception: %s", JsonParserError)
             raise JsonParserError("Json database file is not found") from err
 
     def get_signal_title(self, identifier):
@@ -31,8 +39,13 @@ class JsonParser:
 
         try:
             if service_title == "Null":
+                logger.info("Raising exception: %s", JsonParserError)
                 raise JsonParserError
-        except JsonParserError:
+        except JsonParserError as err:
+            # print(f"Got exception: Signal with ID: {identifier} was not found in database")
+            logger.exception("Exception occurred: %s ", err)
+            logger.error("Requested signal not found in database, signal ID: %s ", identifier)
+            logger.info("Aborting server ... %s")
             abort(404, description="Signal not found")
         return service_title
 
