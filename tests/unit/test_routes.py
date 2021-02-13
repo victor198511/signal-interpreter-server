8b82bed1baecd2df8b11cbb0d8ade982ea9e6f73
Dykin <VDYKIN@volvocars.com>
2021-02-13 20:19:28 +0100

    31 commit

--- tests/unit/test_routes.py
+++ tests/unit/test_routes.py
@@ -1,8 +1,12 @@
 from unittest.mock import patch
 
+# import signal_interpreter_server
+# from signal_interpreter_server.exceptions import JsonParserError
 from signal_interpreter_server.routes import JsonParser
 from signal_interpreter_server.routes import signal_interpreter_app
 
+
+# import pytest
 
 @patch('signal_interpreter_server.routes.abort')
 @patch.object(JsonParser, "get_signal_title")
@@ -19,6 +23,7 @@ def test_interpret_signal(mock_get_signal_title, mock_abort):
         client.post("/", json=my_payload)
         mock_abort.assert_called_once_with(404, description="Signal not found")
 
+
 # @pytest.mark.parametrize("item", [
 #     "Security Access",
 #     "None",
@@ -33,3 +38,20 @@ def test_interpret_signal(mock_get_signal_title, mock_abort):
 #         mock_get_signal_title.return_value = item
 #         response = client.post("/", json=my_payload)
 #         assert response.get_json() == item
+
+# @patch('signal_interpreter_server.routes.abort')
+# @patch.object(JsonParser, "get_signal_title")
+# def test_interpret_signal(mock_get_signal_title, mock_abort):
+#     signal_interpreter_app.testing = True
+#     my_app_instance = signal_interpreter_app.test_client()
+
+#     with my_app_instance as client:
+#         my_payload = {"signal": "27"}
+#         mock_get_signal_title.return_value = "Security Access"
+#         response = client.post("/", json=my_payload)
+#         assert response.get_json() == "Security Access"
+#         mock_get_signal_title.return_value = "None"
+#         client.post("/", json=my_payload)
+#         with pytest.raises(JsonParserError):
+#             signal_interpreter_server.routes.interpret_signal()
+
