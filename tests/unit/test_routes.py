3989d36e6048f2bf03e350db47351597fc59461f
Dykin <VDYKIN@volvocars.com>
2021-02-14 18:22:20 +0100

    32 commit

--- tests/unit/test_routes.py
+++ tests/unit/test_routes.py
@@ -1,12 +1,8 @@
 from unittest.mock import patch
 
-# import signal_interpreter_server
-# from signal_interpreter_server.exceptions import JsonParserError
 from signal_interpreter_server.routes import JsonParser
 from signal_interpreter_server.routes import signal_interpreter_app
 
-
-# import pytest
 
 @patch('signal_interpreter_server.routes.abort')
 @patch.object(JsonParser, "get_signal_title")
@@ -22,36 +18,3 @@ def test_interpret_signal(mock_get_signal_title, mock_abort):
         mock_get_signal_title.return_value = "None"
         client.post("/", json=my_payload)
         mock_abort.assert_called_once_with(404, description="Signal not found")
-
-
-# @pytest.mark.parametrize("item", [
-#     "Security Access",
-#     "None",
-# ])
-# @patch.object(JsonParser, "get_signal_title")
-# def test_interpret_signal1(mock_get_signal_title, item):
-#     signal_interpreter_app.testing = True
-#     my_app_instance = signal_interpreter_app.test_client()
-
-#     with my_app_instance as client:
-#         my_payload = {"signal": "27"}
-#         mock_get_signal_title.return_value = item
-#         response = client.post("/", json=my_payload)
-#         assert response.get_json() == item
-
-# @patch('signal_interpreter_server.routes.abort')
-# @patch.object(JsonParser, "get_signal_title")
-# def test_interpret_signal(mock_get_signal_title, mock_abort):
-#     signal_interpreter_app.testing = True
-#     my_app_instance = signal_interpreter_app.test_client()
-
-#     with my_app_instance as client:
-#         my_payload = {"signal": "27"}
-#         mock_get_signal_title.return_value = "Security Access"
-#         response = client.post("/", json=my_payload)
-#         assert response.get_json() == "Security Access"
-#         mock_get_signal_title.return_value = "None"
-#         client.post("/", json=my_payload)
-#         with pytest.raises(JsonParserError):
-#             signal_interpreter_server.routes.interpret_signal()
-
